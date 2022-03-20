acdc3588a09ff0379168bdc5883ae40ac25d3025
ashishu8888 <81767933+ashishu8888@users.noreply.github.com>
2022-03-20 15:55:48 +0530

    day4 completed

--- flutter_application_1/lib/pages/login_page.dart
+++ flutter_application_1/lib/pages/login_page.dart
@@ -12,7 +12,7 @@ class LoginPage extends StatelessWidget {
           "assets/images/undraw_secure_login_pdn4.png",
           fit: BoxFit.cover,
         ),
-        SizedBox(height: 40),
+        SizedBox(height: 10),
         Text(
           "Welcome",
           style: TextStyle(
@@ -20,17 +20,33 @@ class LoginPage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(height: 20),
-        TextFormField(
-          decoration: InputDecoration(
-            hintText: "Enter username",
-            labelText: "Username",
-          ),
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-            hintText: "Enter password",
-            labelText: "Password",
+        SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter password",
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text("Login"),
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print("logged in");
+                },
+              )
+            ],
           ),
         )
       ],
